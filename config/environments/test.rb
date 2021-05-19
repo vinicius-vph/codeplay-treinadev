0bb2416409a12951ddaecc4211322950eb862838
Vinicius Santos <vinicius.vph@gmail.com>
2021-05-19 12:35:23 -0300

    active storage configured

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -54,4 +54,5 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+  config.active_storage.service = :test 
 end
