4f6155e5c65b68a17b8ffc130d2d62940c0ad1d3
Vinicius Santos <vinicius.vph@gmail.com>
2021-05-19 12:48:40 -0300

    developed features based on the tests

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,11 @@
 class CoursesController < ApplicationController
+    before_action :set_course, only: %i[show edit update destroy]
+
     def index
         @courses = Course.all
     end
 
     def show
-        @course = Course.find(params[:id])
     end
 
     def new
@@ -21,9 +22,28 @@ class CoursesController < ApplicationController
         end
     end
 
+    def edit
+    end
+    
+    def update
+        @course.update(course_params)
+        redirect_to @course, notice: 'Curso atualizado com sucesso'
+    end
+
+    def destroy
+        @course.destroy 
+        redirect_to courses_path, notice: 'Curso apagado com sucesso!'
+    end
+
     private
     
+    def set_course
+        @course = Course.find(params[:id])
+    end
+
     def course_params
-        params.require(:course).permit(:name, :description, :code, :price, :enrollment_deadline)
+        params
+            .require(:course)
+            .permit(:name, :description, :code, :price, :enrollment_deadline, :banner)
     end
 end 
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ApplicationRecord
+    has_one_attached :banner
     validates :name, :code, :price, presence: { message: 'não pode ficar em branco' }
     validates :code, uniqueness: { message: 'já está em uso' }
 end
