22e77d9d735b9f0fcab61020e825c10e1ee2d2a3
Vinicius Santos <vinicius.vph@gmail.com>
2021-05-19 12:46:41 -0300

    admin deletes tests configured

---
+++ spec/system/admin_deletes_courses_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'admin deletes courses' do
+    it 'successfully' do
+        course = Course.create!(name: 'Ruby', description: 'Um curso de Ruby',
+                                 code: 'RUBYBASIC', price: 10,
+                                 enrollment_deadline: '22/12/2033')
+
+
+        visit course_path(course)
+        
+        expect { click_on 'Apagar' }.to change {Course.count}.by(-1)
+        expect(page).to have_text('Curso apagado com sucesso!')
+        expect(current_path).to have_text(courses_path)
+    end
+end
