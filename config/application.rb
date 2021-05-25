9f757f631ca13bae5da2f2f95db09906d7821d5c
Vinicius Santos <vinicius.vph@gmail.com>
2021-05-25 16:41:40 -0300

    added i18n on project

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Codeplay
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.time_zone = "Brasilia"
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
