f0c5c5da065b8cb7e29ee67e01e9a5cc1052bd2c
Vinicius Santos <vinicius.vph@gmail.com>
2021-05-28 23:17:59 -0300

    its possible to see lessons details now

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,16 +1,25 @@
 class LessonsController < ApplicationController
     before_action :set_course, only: %i[new create]
+    before_action :set_lesson, only: %i[show]
 
     def new
         @lesson = Lesson.new
     end
-    
+
+    def show
+    end
+
     def create
         @lesson = @course.lessons.create!(lesson_params)
         redirect_to @course, notice: 'Aula cadastrada com sucesso'
     end
 
     private
+
+    def set_lesson
+        @lesson = Lesson.find(params[:id])
+    end
+    
     def set_course
         @course = Course.find(params[:course_id])
     end
