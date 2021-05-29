a431852bade70a4cc5dab3b4ca47f5b55e069cc3
Vinicius Santos <vinicius.vph@gmail.com>
2021-05-29 01:20:38 -0300

    instructor tests met

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,6 +1,8 @@
 class Instructor < ApplicationRecord
     has_many :courses
 
+    validates :name, :email, presence: true
+    
     def display_name
         "#{name} - #{email}"
     end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Lesson, type: :model do
+describe Lesson do
+  it { should belong_to(:course) }
+  it { should validate_numericality_of(:duration).only_integer.is_greater_than(0) }
 end
