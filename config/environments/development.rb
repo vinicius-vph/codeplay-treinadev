7a0839365abdd42219be79f9ec95d26fec905d6b
Vinicius Santos <vinicius.vph@gmail.com>
2021-05-30 11:25:08 -0300

    add devise gem | register and loggin tests met

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,10 +10,10 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  
   # Show full error reports.
   config.consider_all_requests_local = true
-
+  
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
@@ -26,9 +26,11 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
-
+    
     config.cache_store = :null_store
   end
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
