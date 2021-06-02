29ebede61e250c1156a3a958ec7fada77a75c300
Vinicius Santos <vinicius.vph@gmail.com>
2021-06-02 04:48:06 -0300

    css adjusts

--- spec/system/visitor_visit_homepage_spec.rb
+++ spec/system/visitor_visit_homepage_spec.rb
@@ -5,7 +5,7 @@ describe 'Visitor visit homepage' do
         visit root_path
 
         expect(page).to have_css('h1', text: 'Codeplay')
-        expect(page).to have_css('h2', text: 'Boas vindas ao sistema de gestão de '\
+        expect(page).to have_css('h3', text: 'Boas vindas ao sistema de gestão de '\
                                         'cursos e aulas')
   end
 end
